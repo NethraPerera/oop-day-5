0260c5019dbada912bc82b9cc0623b5ff72ea0ec
NethraPerera <mr.esanda2001@gmail.com>
2026-07-12 09:22:35 +0530

    Q158

--- Demo.java
+++ Demo.java
@@ -8,6 +8,24 @@ class Demo{
 		Node n1=new Node(100);
 		n1.next=new Node(200);
 		n1.next.next=new Node(300);
-		n1.next.next.next=n1;
+		n1.next.next.next=new Node(400);
+		n1.next.next.next.next=new Node(500);
+		
+		//prints data- [100, 200, 300, 400, 500] using a while loop
+		System.out.print("[");
+		while (n1!= null){
+			System.out.print(n1.data+",");
+			n1=n1.next;
+		}
+		System.out.print("\b]");
+		/*
+		System.out.print("[");
+		Node temp=n1;
+		while (temp!= null){
+			System.out.print(temp.data+",");
+			temp=temp.next;
+		}
+		System.out.print("\b]");
+		*/
 	}
 }
